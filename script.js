b89897c300307dec975768e41c2a90e11219d500
williisme <williamxkane@gmail.com>
2026-06-14 18:46:43 -0400

    Add script.js with basic setup

---
+++ script.js
@@ -0,0 +1,9 @@
+// JavaScript code for your project
+console.log('My Cool Check is running!');
+
+// Example: Add interactivity
+document.addEventListener('DOMContentLoaded', function() {
+    console.log('Page loaded and ready to go!');
+    
+    // Your code here
+});
